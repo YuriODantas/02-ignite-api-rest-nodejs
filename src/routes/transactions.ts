9928ba3165ca85a35ab5e77be49ce82db2a04761
YuriODantas <iruy3343@gmail.com>
2024-02-23 11:07:25 -0300

    feat: create route post by create a new transaction

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,22 +1,25 @@
 import { FastifyInstance } from 'fastify'
 import { knex } from '../database'
+import { z } from 'zod'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/transactions', async () => {
-    const transactions = await knex('transactions').select('*')
+  app.post('/', async (request, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
 
-    return transactions
-  })
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body,
+    )
 
-  app.get('/hello', async () => {
-    const transaction = await knex('transactions')
-      .insert({
-        id: crypto.randomUUID(),
-        title: 'transação de teste',
-        amount: 1000,
-      })
-      .returning('*')
+    await knex('transactions').insert({
+      id: crypto.randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
 
-    return transaction
+    return reply.status(201).send()
   })
 }
